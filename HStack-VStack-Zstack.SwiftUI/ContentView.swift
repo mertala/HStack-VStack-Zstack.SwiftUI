9b7c509d8b92fdf88e7a2273e7adaf5197fc4718
Mert <mertala@icloud.com>
2019-10-11 21:43:17 +0300

    Initial Commit

---
+++ HStack-VStack-Zstack.SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HStack-VStack-Zstack.SwiftUI
+//
+//  Created by Mert Ala on 11.10.2019.
+//  Copyright © 2019 Mert Ala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
